250467815a7b43e578dfe68f4e6b744bfe1a0582
Rahul1656 <rj845854@gmail.com>
2021-06-06 11:09:12 +0530

    Updated

---
+++ Activity1_classes_and_objects/src/com/Ltts/Employee.java
@@ -0,0 +1,27 @@
+package com.Ltts;
+
+public class Employee 
+{
+  private String name;
+  private String adress;
+  private String mobile;
+  
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+public String getMobile() {
+	return mobile;
+}
+public void setMobile(String mobile) {
+	this.mobile = mobile;
+}  
+}
